c4e715dff419536e4104bca89f2eb38cd8dc1cfc
Zahidkhanjs <zahidshahwani9925@gmail.com>
2026-05-05 18:01:19 +0500

    dome initlize

---
+++ 05-Array/app.js
@@ -0,0 +1,145 @@
+
+
+// let fruit1 = "apple"
+// let fruit2 = "mengo" 
+// let fruit3 = "banana "
+
+// console.log(fruit1 + fruit2 + fruit3);
+
+// var student1 = "zahid"
+// var student2 = "khan"
+
+// var students =["zahid" , "khan" , "one "]
+
+// students[3] = "zahidkhan"  // adding new element
+
+// console.log(students);
+
+
+// get array leangth 
+
+// var students = []
+
+// students[students.length] ="zahidkhan"
+// students [students.length] = "zain"
+
+// console.log(students.length); 
+
+
+// var students = ["zahid " , "khan "]
+
+// students[5] = "Ali"
+
+// console.log(students);
+
+
+// var students = ["zahidkhan" , "salman gul " ,]
+
+// // students.pop()  //Removing element at the end
+
+// var returnvale = students.pop();
+// console.log("returnevalue");
+
+// console.log(students);
+
+
+// var student = ["zahid khan" , "zamzam"]
+
+
+
+// var returnevale = student.pop()
+// console.log(returnevale);
+
+// console.log(student);
+
+
+
+// var username = ["usman " , "abdullah" , "kamran khan"]
+
+// var returnevale = username.pop()  // showing in the cosole reture vale 
+
+// console.log(returnevale);
+
+// console.log(username);
+
+
+
+// var username = ["zahidkhan" , "baloch" , "laptop"]
+
+// var returnevlaue = username.push("baluchistan" , "zain ali")
+// console.log(returnevlaue);
+
+
+
+// console.log(username );
+
+
+// var username = ["zahidkhan" , "hashim"]
+
+// var returnevale = username.shift()
+// console.log(returnevale);
+// console.log(username);
+
+// var username = [ "zahid " , "khan " , "hello "]
+
+// username.splice(0 , 0 , "zhaidhakhkjh" )
+// console.log(username);
+
+
+// var username = [ "zahid khan" , "hello " , "fruit"]
+
+// var copyary = username.slice(1,2)
+
+// console.log(copyary);
+// console.log(username);
+
+// var fruits = ["apple" , "mengo " , "banana "]
+
+// var newfruit = fruits.slice(1,2)
+
+// console.log(newfruits);
+// console.log(fruits);
+
+
+// let numbers = [10, 20, 30, 40, 50, 60];
+// let num = numbers.slice(2,5)
+// console.log(numbers);
+// console.log(num);
+
+// let colors = ["red", "blue", "green", "yellow", "black"];
+// let yello = colors.slice(3 , 6)
+// console.log(colors);
+// console.log(yello);
+
+
+// let marks = [80, 90, 70, 60];
+// let copy = marks.slice(0 , 4)
+// console.log(marks);
+// console.log(copy);
+
+// let animals = ["lion", "cat", "dog", "tiger", "elephant"];
+// var janver = animals.slice(1 ,3)
+// console.log(animals);
+// console.log(janver);
+
+// let fruits = ["apple", "banana", "mango", "orange", "grapes"];
+// let vegetables = fruits.slice(0,3)
+// console.log(fruits);
+// console.log(vegetables);
+
+// let fruits = ["apple", "banana", "mango"];
+// let origin = fruits.push(origin)
+// console.log(fruits);
+
+
+// let removed = "mengo"
+// console.log(removed);
+
+["Ali", "Ahmed", "Bilal"]
+
+
+
+let firstname = [ "ali" , "Ahme" , "Bilal"]
+let username = name.shift("ali")
+console.log(firstname);
+console.log(username);
